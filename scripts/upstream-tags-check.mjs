3fd5e6dbbb7754b39a0218683cb52b6c85f90faa
yaegassy <yaegassy@users.noreply.github.com>
2023-06-27 16:25:13 +0900

    chore: tweak of tags-check script

--- scripts/upstream-tags-check.mjs
+++ scripts/upstream-tags-check.mjs
@@ -1,12 +1,15 @@
 import fs from 'node:fs';
 
+const FEATCH_URL = 'https://api.github.com/repos/microsoft/vscode-mypy/tags';
+const RESOURCE_FILE_NAME = 'vscode-mypy-tags.json';
+
 await (async () => {
-  const res = await fetch('https://api.github.com/repos/microsoft/vscode-mypy/tags');
+  const res = await fetch(FEATCH_URL);
   if (!res.ok) return;
   const remoteJson = await res.json();
   const remoteJsonStr = JSON.stringify(remoteJson, null, 2);
 
-  const resourcePath = new URL('../resources/vscode-mypy-tags.json', import.meta.url).pathname;
+  const resourcePath = new URL(`../resources/${RESOURCE_FILE_NAME}`, import.meta.url).pathname;
   let localJsonStr = await fs.promises.readFile(resourcePath, { encoding: 'utf-8' });
   const localJson = JSON.parse(localJsonStr);
   localJsonStr = JSON.stringify(localJson, null, 2);
